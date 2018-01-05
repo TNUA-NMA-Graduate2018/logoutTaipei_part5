39e2ef2e711482df1a6f198a4a3005ce0be58ae5
Yungga <cherry841027@gmail.com>
2018-01-05 14:32:28 +0800

    Add LED part
    可變電阻版

--- RadioVersion/uno_server/uno_server.ino
+++ RadioVersion/uno_server/uno_server.ino
@@ -1,6 +1,14 @@
 #include <SPI.h>
 #include "RF24.h"
 
+#include <Adafruit_NeoPixel.h>
+
+int pixelNumber = 120;
+Adafruit_NeoPixel stripA = Adafruit_NeoPixel(pixelNumber, 5, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripB = Adafruit_NeoPixel(pixelNumber, 6, NEO_GRB + NEO_KHZ800);
+
+int value = 1;
+
 RF24 rf24(7, 8); // CE腳, CSN腳
 
 const byte addr[] = "1Node";
@@ -18,9 +26,10 @@ void setup() {
   rf24.setPALevel(RF24_PA_MIN);   // 設定廣播功率
   rf24.setDataRate(RF24_250KBPS); // 設定傳輸速率
   rf24.stopListening();       // 停止偵聽；設定成發射模式
-
+  
   pinMode(readSlider1, INPUT);
   pinMode(readSlider2, INPUT);
+  pinMode(A2,INPUT)//可變電阻調LED
 
   rf24.begin();
   rf24.setChannel(83);       // 設定頻道編號
@@ -28,12 +37,23 @@ void setup() {
   rf24.setPALevel(RF24_PA_MIN);   // 設定廣播功率
   rf24.setDataRate(RF24_250KBPS); // 設定傳輸速率
   rf24.stopListening();       // 停止偵聽；設定成發射模式
+
+  stripA.begin();
+  stripB.begin();
 }
 
 void loop() {
   ToOtherRight = slider(readSlider1);
   ToOtherLeft = slider(readSlider2);
   SendClient(ToOtherLeft, ToOtherRight);
+
+   switch (value) {
+
+    case 1: LedChangeOhyeah (1); break;
+    case 2: LedChangeOhyeah (2); break;
+    case 3: LedChangeOhyeah (3); break;
+
+  }
 }
 
 
@@ -79,3 +99,97 @@ int slider(int slider) {
   delay(100);
   return value ;
 }
+
+
+
+void   LedChangeOhyeah (int num) {
+  if (num == 1) {
+    theaterChase(50, 5);//50是亮度 5是等多久(速度)
+  }
+  else if (num == 2) {
+    breath(20, 80);
+    stripA.show();
+    stripB.show();
+  }
+  else if (num == 3) {
+    rainbowCycle(50);
+  }
+  if (value != valueold) {
+    valueold = value;
+    for (int n = 0; n < stripA.numPixels(); n++) {
+      stripA.setPixelColor(n, stripA.Color(0, 0, 0));
+      stripB.setPixelColor(n, stripB.Color(0, 0, 0));
+    }
+    stripA.show();
+    stripB.show();
+  }
+}
+void theaterChase(int c, uint8_t wait) {
+  for (int j = 0; j < 1; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 2; q++) {
+      for (int i = 0; i < 120 + 20; i = i + 10) {
+        for (int a = 0; a < 10; a++) {
+          stripA.setPixelColor(i + q - a, stripA.Color(c - (5 * (a + 1)), c - (5 * (a + 1)), c - (5 * (a + 1))));
+          stripA.setPixelColor(i + q - 9 - a, stripA.Color(0, 0, 0));
+          
+          stripB.setPixelColor(i + q - a, stripB.Color(c - (5 * (a + 1)), c - (5 * (a + 1)), c - (5 * (a + 1))));
+          stripB.setPixelColor( i+ q - 9 - a, stripB.Color(0, 0, 0));
+          delay(wait);
+        }//turn every third pixel on
+        stripA.show();
+        stripB.show();
+      }
+    }
+  }
+}
+void breath(int c, uint8_t wait) {
+  int i, j;
+
+  for (j = 0; j < c+1; j++) {
+    for (i = 0; i < stripA.numPixels(); i++) {
+      stripA.setPixelColor(i, stripA.Color(c - j, c - j, c - j));
+      stripB.setPixelColor(i, stripB.Color(c - j, c - j, c - j));
+
+    }
+    stripA.show();
+    stripB.show();
+    delay(wait);
+  }
+  for (j = 0; j < c+1; j++) {
+    for (i = 0; i < stripA.numPixels(); i++) {
+      stripA.setPixelColor(i, stripA.Color(j, j, j));
+      stripB.setPixelColor(i, stripB.Color(j, j, j));
+    }
+    stripA.show();
+    stripB.show();
+    delay(wait);
+  }
+}
+
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 128; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < stripA.numPixels(); i++) {
+      stripA.setPixelColor(i, Wheel(((i * 255 / stripA.numPixels()) + j ) & 127));
+      stripB.setPixelColor(i, Wheel(((i * 255 / stripB.numPixels()) + j ) & 127));
+    }
+    stripA.show();
+    stripB.show();
+    //delay(wait);
+  }
+}
+
+
+uint32_t Wheel(byte WheelPos) {
+  if (WheelPos < 42) {
+    return stripA.Color(WheelPos * 3, 127 - WheelPos * 3, 0);
+  } else if (WheelPos < 85) {
+    WheelPos -= 42;
+    return stripA.Color(127 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+    WheelPos -= 85;
+    return stripA.Color(0, WheelPos * 3, 127 - WheelPos * 3);
+  }
+}
+
