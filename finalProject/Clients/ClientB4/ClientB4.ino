8c2bc992c1106ce78cef720d2ec7e31f787a7819
aprilcoffee <8765498@gmail.com>
2018-03-23 03:53:16 +0800

    TG update

--- finalProject/Clients/ClientB4/ClientB4.ino
+++ finalProject/Clients/ClientB4/ClientB4.ino
@@ -3,8 +3,8 @@
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 
-int pixelNumber = 30;//記得改成正確的
-int mid = 15;//記得改成正確的
+int pixelNumber = 20;//記得改成正確的
+int mid = 10;//記得改成正確的
 
 RF24 rf24(9, 10); // CE腳, CSN腳
 const byte addr[] = "4Node";
--- finalProject/Clients/ClientB8/ClientB8.ino
+++ finalProject/Clients/ClientB8/ClientB8.ino
@@ -3,11 +3,11 @@
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 
-int pixelNumber = 30;//記得改成正確的
-int mid = 15;//記得改成正確的
+int pixelNumber = 20;//記得改成正確的
+int mid = 10;//記得改成正確的
 
 RF24 rf24(9, 10); // CE腳, CSN腳
-const byte addr[] = "7Node";
+const byte addr[] = "8Node";
 byte pipe = 1;  // 指定通道編號
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixelNumber, 2, NEO_GRB + NEO_KHZ800);
 boolean mode = 1;
@@ -42,7 +42,7 @@ void setup() {
   pinMode(LedL, OUTPUT);
 
   rf24.begin();
-  rf24.setChannel(100);  // 設定頻道編號
+  rf24.setChannel(105);  // 設定頻道編號
   rf24.setPALevel(RF24_PA_LOW);
   rf24.setDataRate(RF24_250KBPS);
   rf24.openReadingPipe(pipe, addr);  // 開啟通道和位址
