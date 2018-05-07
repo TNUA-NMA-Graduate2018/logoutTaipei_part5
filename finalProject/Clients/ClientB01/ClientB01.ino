55fd6a4ed1568836b725bcff7e34cf12b6e4e5fa
aprilcoffee <8765498@gmail.com>
2018-05-07 20:53:48 +0800

    update starting by remotre

--- finalProject/Clients/ClientB01/ClientB01.ino
+++ finalProject/Clients/ClientB01/ClientB01.ino
@@ -10,8 +10,8 @@ RF24 rf24(9, 10); // CE腳, CSN腳
 const byte addr[] = "1Node";
 byte pipe = 1;  // 指定通道編號
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixelNumber, 2, NEO_GRB + NEO_KHZ800);
-boolean mode = 1;
-boolean modeChange = 1; //0=遙控 1=互控
+boolean mode = 0;
+boolean modeChange = 0; //0=遙控 1=互控
 //const int modeChanging = 3;
 int countNoSignal = 0;
 
@@ -88,7 +88,7 @@ void controlByOther(int FromOtherL, int FromOtherR) {//0後退 1不動 2前進
     }
     if (fromOtherRFixed != fromOtherR || fromOtherLFixed != fromOtherL) {
       breath(fromOtherL, fromOtherR);
-      delay(200);
+      delay(300);
     }
   }
   fromOtherRFixed = fromOtherR;
@@ -139,8 +139,8 @@ void connectCheck() {
     }
     //偵測方向 2後退 1停止 0前進
     if (mode == 0) {
-      //countNoSignal = 0;
       if (mg[0] == 'R') {
+        countNoSignal = 0;
         fromOtherL = mg[1] - '0';
         fromOtherR = mg[2] - '0';
       }
@@ -150,8 +150,7 @@ void connectCheck() {
         countNoSignal = 0;
         fromOtherL = mg[1] - '0';
         fromOtherR = mg[2] - '0';
-      }
-      if (mg[0] == 'R') {
+      } if (mg[0] == 'R') {
         countNoSignal = 0;
       }
     }
--- finalProject/Clients/ClientB11/ClientB11.ino
+++ finalProject/Clients/ClientB11/ClientB11.ino
@@ -10,8 +10,8 @@ RF24 rf24(9, 10); // CE腳, CSN腳
 const byte addr[] = "BNode";
 byte pipe = 1;  // 指定通道編號
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixelNumber, 2, NEO_GRB + NEO_KHZ800);
-boolean mode = 1;
-boolean modeChange = 1; //0=遙控 1=互控
+boolean mode = 0;
+boolean modeChange = 0; //0=遙控 1=互控
 //const int modeChanging = 3;
 int countNoSignal = 0;
 
